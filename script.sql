ebdd550c22bbcd84bc2e56a3546a4c70b88d9a2c
Basant kumar Hota <basahota@users.noreply.github.com>
2017-09-19 23:56:38 +0530

    Upload DB Script

---
+++ script.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM onlinetest.onlineqa
+LIMIT 0, 1000
+
+-- Date: 2017-09-19 23:55
+*/
+INSERT INTO `onlineqa` (`questionId`,`option1`,`option2`,`option3`,`option4`,`question`,`validAnswer`,`question_id`,`valid_answer`) VALUES (1,'Polymorphisim','Abstraction','Encapsulation','Method hiding','Which is not part OOP principle ?','Method hiding',0,NULL);
+INSERT INTO `onlineqa` (`questionId`,`option1`,`option2`,`option3`,`option4`,`question`,`validAnswer`,`question_id`,`valid_answer`) VALUES (2,'try-catch','try-finally','try','None of them','Which is valid syntax to handel Exception','try-catch',0,NULL);
+INSERT INTO `onlineqa` (`questionId`,`option1`,`option2`,`option3`,`option4`,`question`,`validAnswer`,`question_id`,`valid_answer`) VALUES (3,'valueOf','intValue','getInt','getInteger','Select the one correct answer. Which method defined in Integer class can be used to convert an Integer object to primitive int type. ','intValue',0,NULL);
+INSERT INTO `onlineqa` (`questionId`,`option1`,`option2`,`option3`,`option4`,`question`,`validAnswer`,`question_id`,`valid_answer`) VALUES (4,'test() { }','Test() { }','void Test() { }','private final Test() { }','Which of the following are valid constructors within a class Test. Select the two correct answers.','Test() { }',0,NULL);
+INSERT INTO `onlineqa` (`questionId`,`option1`,`option2`,`option3`,`option4`,`question`,`validAnswer`,`question_id`,`valid_answer`) VALUES (5,'Tree','Array','LinkedList','Map','Which of these are core interfaces in the collection framework. Select the one correct answer.','Map',0,NULL);
